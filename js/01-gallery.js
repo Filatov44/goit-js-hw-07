c8b8548b2537d979e7c81c97133342355d45dc16
Filatov44 <filatov300@gmail.com>
2022-06-18 18:42:44 +0300

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const renderListGalleryEl = renderListGallery(galleryItems);
 
-console.log(galleryItems);
+// Создаем разметку из массива объектов
+
+function renderListGallery(items) {
+  return items.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+    ""
+  );
+}
+
+// Вставляем разметку на экран
+
+galleryEl.insertAdjacentHTML("beforeend", renderListGalleryEl);
+
+// Прослушиваем события на галерее
+
+galleryEl.addEventListener("click", openModalGalleryWindow);
+
+function openModalGalleryWindow(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" alt="${event.target.getAttribute(
+    "alt"
+  )}" >
+    `);
+
+  instance.show();
+}
